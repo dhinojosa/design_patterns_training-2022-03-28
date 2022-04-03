14d13eb5717ac5c448282d4699fce19ef8cc349d
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2022-04-03 10:33:21 -0600

    Updated from class.

--- src/main/java/com/xyzcorp/javapatterns/interpreter/functional/Expression.java
+++ src/main/java/com/xyzcorp/javapatterns/interpreter/functional/Expression.java
@@ -1,6 +1,6 @@
 package com.xyzcorp.javapatterns.interpreter.functional;
 
-public sealed interface Expression permits Constant, Sum, Subtract, Multiply {
+public sealed interface Expression permits Constant, Sum, Subtract, Multiply, Negative {
 }
 
 
---
+++ src/main/java/com/xyzcorp/javapatterns/interpreter/functional/Negative.java
@@ -0,0 +1,4 @@
+package com.xyzcorp.javapatterns.interpreter.functional;
+
+public record Negative(Expression expression) implements Expression{
+}
